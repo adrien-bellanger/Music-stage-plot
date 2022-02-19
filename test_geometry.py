606ec461989f379c1f6ea9d9c855c6136fe3843d
Adrien Bellanger <darktable@bellanger-fr.de>
2022-02-19 18:13:31 +0100

    First test

---
+++ test_geometry.py
@@ -0,0 +1,20 @@
+import math
+import unittest
+
+import geometry
+
+
+class TestCircle(unittest.TestCase):
+    def test_something(self):
+        c1: geometry.Circle = geometry.Circle(geometry.Position.from_xy(0, 0), 100)
+        c2: geometry.Circle = geometry.Circle(geometry.Position.from_xy(0, 100), math.sqrt(2) * 100)
+        p1: geometry.Position = c1.intersections(c2, False)
+        p2: geometry.Position = c1.intersections(c2, True)
+        self.assertAlmostEqual(0, p1.point.y, 6)
+        self.assertAlmostEqual(-100, p1.point.x, 6)
+        self.assertAlmostEqual(0, p2.point.y, 6)
+        self.assertAlmostEqual(100, p2.point.x, 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
