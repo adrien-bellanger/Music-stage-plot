56249df2def8cfa7af7f20454bc0c99c6eabacd4
Adrien Bellanger <darktable@bellanger-fr.de>
2022-03-05 10:11:30 +0100

    Tests

--- test_geometry.py
+++ test_geometry.py
@@ -11,9 +11,9 @@ class TestCircle(unittest.TestCase):
         p1: geometry.Position = c1.intersections(c2, False)
         p2: geometry.Position = c1.intersections(c2, True)
         self.assertAlmostEqual(0, p1.point.y, 6)
-        self.assertAlmostEqual(-100, p1.point.x, 6)
+        self.assertAlmostEqual(100, p1.point.x, 6)
         self.assertAlmostEqual(0, p2.point.y, 6)
-        self.assertAlmostEqual(100, p2.point.x, 6)
+        self.assertAlmostEqual(-100, p2.point.x, 6)
 
 
 if __name__ == '__main__':
